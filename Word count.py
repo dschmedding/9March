dce63c2c1c9ab92cb25f06862e04ead61b6b3cee
dschmedding <daniel.schmedding@student.ie.edu>
2020-03-09 15:11:27 +0100

    Homework

---
+++ Word count.py
@@ -0,0 +1,24 @@
+f = open("/Users/dschmedding/PycharmProjects/9March/Harry Potter", "r")
+data = f.read()
+f.close()
+words = data.split(" ")
+numb_words = len(words)
+numb_words = int(numb_words)
+print("The number of words is", numb_words)
+
+f2 = open("/Users/dschmedding/PycharmProjects/9March/Dracula", "r")
+data2 = f2.read()
+words2 = data2.split(" ")
+numb_words2 = len(words)
+numb_words2 = int(numb_words2)
+print("The number of words in the second text is", numb_words2)
+
+if numb_words > numb_words2:
+    print("Harry Potter has more words than Dracula")
+if numb_words2 > numb_words:
+    print("Dracula has more different words than Harry Potter")
+
+
+
+
+
